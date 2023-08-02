26b5c696f968e605f43062a88c79de77ebdbd0e3
Bob Freid <17238974+AirPro@users.noreply.github.com>
2023-08-02 15:24:41 -0400

    Created an Amplifier Controller.Added Guitars and Pedals Controllers

---
+++ FinalProjectExampleOne/Controllers/GuitarsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectExampleOne.Data;
+using FinalProjectExampleOne.Models;
+
+namespace FinalProjectExampleOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuitarsController : ControllerBase
+    {
+        private readonly ContactsAPIDBContext _context;
+
+        public GuitarsController(ContactsAPIDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Guitars
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Guitar>>> GetGuitars()
+        {
+          if (_context.Guitars == null)
+          {
+              return NotFound();
+          }
+            return await _context.Guitars.ToListAsync();
+        }
+
+        // GET: api/Guitars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Guitar>> GetGuitar(Guid id)
+        {
+          if (_context.Guitars == null)
+          {
+              return NotFound();
+          }
+            var guitar = await _context.Guitars.FindAsync(id);
+
+            if (guitar == null)
+            {
+                return NotFound();
+            }
+
+            return guitar;
+        }
+
+        // PUT: api/Guitars/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGuitar(Guid id, Guitar guitar)
+        {
+            if (id != guitar.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(guitar).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GuitarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Guitars
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Guitar>> PostGuitar(Guitar guitar)
+        {
+          if (_context.Guitars == null)
+          {
+              return Problem("Entity set 'ContactsAPIDBContext.Guitars'  is null.");
+          }
+            _context.Guitars.Add(guitar);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGuitar", new { id = guitar.Id }, guitar);
+        }
+
+        // DELETE: api/Guitars/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGuitar(Guid id)
+        {
+            if (_context.Guitars == null)
+            {
+                return NotFound();
+            }
+            var guitar = await _context.Guitars.FindAsync(id);
+            if (guitar == null)
+            {
+                return NotFound();
+            }
+
+            _context.Guitars.Remove(guitar);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GuitarExists(Guid id)
+        {
+            return (_context.Guitars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FinalProjectExampleOne/Controllers/PedalsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectExampleOne.Data;
+using FinalProjectExampleOne.Models;
+
+namespace FinalProjectExampleOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedalsController : ControllerBase
+    {
+        private readonly ContactsAPIDBContext _context;
+
+        public PedalsController(ContactsAPIDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pedals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pedal>>> GetPedals()
+        {
+          if (_context.Pedals == null)
+          {
+              return NotFound();
+          }
+            return await _context.Pedals.ToListAsync();
+        }
+
+        // GET: api/Pedals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedal>> GetPedal(Guid id)
+        {
+          if (_context.Pedals == null)
+          {
+              return NotFound();
+          }
+            var pedal = await _context.Pedals.FindAsync(id);
+
+            if (pedal == null)
+            {
+                return NotFound();
+            }
+
+            return pedal;
+        }
+
+        // PUT: api/Pedals/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPedal(Guid id, Pedal pedal)
+        {
+            if (id != pedal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pedal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pedals
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pedal>> PostPedal(Pedal pedal)
+        {
+          if (_context.Pedals == null)
+          {
+              return Problem("Entity set 'ContactsAPIDBContext.Pedals'  is null.");
+          }
+            _context.Pedals.Add(pedal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPedal", new { id = pedal.Id }, pedal);
+        }
+
+        // DELETE: api/Pedals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePedal(Guid id)
+        {
+            if (_context.Pedals == null)
+            {
+                return NotFound();
+            }
+            var pedal = await _context.Pedals.FindAsync(id);
+            if (pedal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pedals.Remove(pedal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PedalExists(Guid id)
+        {
+            return (_context.Pedals?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
